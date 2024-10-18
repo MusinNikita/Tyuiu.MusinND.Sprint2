2c5372f82975530a76de36d3c764b8a54b6ffb46
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-18 08:23:29 +0500

    1212

---
+++ Tyuiu.MusinND.Sprint2.Task7.V14.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.MusinND.Sprint2.Task7.V14.Lib
+{
+    public class DataService : ISprint2Task7V14
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            
+            bool insideCircle = (x * x + y * y) <= 1;
+            bool insideRegion = (y >= -x) && (y <= x);
+
+            if (insideCircle && insideRegion)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
